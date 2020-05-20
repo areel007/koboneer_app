c615fa38f2aeb44c158859bc31890ba3ab12ee1d
areel007 <morenikejisunday12@gmail.com>
2020-05-20 02:51:36 +0100

    first commit

---
+++ build/precache-manifest.468b8f491c2354384375adc71e87bc32.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d82d59aa068298881b6d5f1fbdf1f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "115b9f8da76fd8215b2f",
+    "url": "/static/css/main.e3266209.chunk.css"
+  },
+  {
+    "revision": "0cfb9f8989118a65482b",
+    "url": "/static/js/2.d58a7557.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d58a7557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115b9f8da76fd8215b2f",
+    "url": "/static/js/main.e7b42a1a.chunk.js"
+  },
+  {
+    "revision": "ae2c1dfde1e76d939ce7",
+    "url": "/static/js/runtime-main.cc9a0243.js"
+  },
+  {
+    "revision": "92b4e748ad77f8f180d4321bced11b30",
+    "url": "/static/media/2.92b4e748.svg"
+  },
+  {
+    "revision": "836a89bad368f80e1c7dea96de456f11",
+    "url": "/static/media/7.836a89ba.svg"
+  },
+  {
+    "revision": "61c7aa614d5febd3ee43456807a34921",
+    "url": "/static/media/address.61c7aa61.svg"
+  },
+  {
+    "revision": "9a30c75b0d9aaa2a7ff3eb9d1130b9e8",
+    "url": "/static/media/buy.9a30c75b.svg"
+  },
+  {
+    "revision": "53757d9306796cd8c19df677d30a10c7",
+    "url": "/static/media/fb.53757d93.svg"
+  },
+  {
+    "revision": "ec5bc1b3148ca464a3a3e17af89c3ead",
+    "url": "/static/media/github.ec5bc1b3.svg"
+  },
+  {
+    "revision": "1e01f0c0dea217ba568ebbcec49c5b6b",
+    "url": "/static/media/in.1e01f0c0.svg"
+  },
+  {
+    "revision": "b25e5056cc906cf49f1e5e3a490eca5a",
+    "url": "/static/media/mail.b25e5056.svg"
+  },
+  {
+    "revision": "945d571c6e86579fa4bd81f7cc58f00b",
+    "url": "/static/media/mobile.945d571c.svg"
+  },
+  {
+    "revision": "f3b7d629c80a85788fe1cc7377576fec",
+    "url": "/static/media/phone.f3b7d629.svg"
+  },
+  {
+    "revision": "6620c8ae2cac90067a9aa78339bb808d",
+    "url": "/static/media/twitter.6620c8ae.svg"
+  },
+  {
+    "revision": "fd0074f9adf50729a73931a9dceaf27a",
+    "url": "/static/media/welcome.fd0074f9.svg"
+  }
+]);
